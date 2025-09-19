3b76bb0a628bd1ee0ea3f28c98df1bdb19b290a7
megas-mdn <mdn.melqui@gmail.com>
2025-09-19 19:29:40 -0300

    feat: GET caixas

--- src/packing/packing.controller.ts
+++ src/packing/packing.controller.ts
@@ -1,16 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { PackingService } from './packing.service';
 import { PackRequestDTO } from './dto/pack-request.dto';
 import { PackResponseDTO } from './dto/pack-response.dto';
+import { Caixa } from './entities/caixa.entity';
 
 @ApiTags('Packing')
 @Controller('packing')
 export class PackingController {
   constructor(private readonly packingService: PackingService) {}
 
+  // POST -> processar pedidos
   @Post()
   processar(@Body() payload: PackRequestDTO): PackResponseDTO {
     return this.packingService.processarPedidos(payload);
+  }
+
+  // GET -> listar caixas disponíveis (fakeDB)
+  @Get('/')
+  listarCaixas(): Caixa[] {
+    return this.packingService.getCaixas();
   }
 }
