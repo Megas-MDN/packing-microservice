a6bd1e555cace815b2829168849b226468054aee
megas-mdn <mdn.melqui@gmail.com>
2025-09-19 19:51:07 -0300

    feat: Cors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Validação global para os DTOs
+  // Validação global usando os DTOs
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
 
-  await app.listen(process.env.PORT ?? 3001);
+  app.enableCors();
+
+  //  Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Packing API')
+    .setDescription('API para empacotar produtos nas caixas disponíveis')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document); // <== define a rota /doc
+
+  // Porta da API
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT);
+  console.log(`API rodando em: http://localhost:${PORT}`);
+  console.log(`Swagger disponível em: http://localhost:${PORT}/doc`);
 }
 bootstrap();
